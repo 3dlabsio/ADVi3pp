f3f4c8fa9867ec0e98a13f531eb0b277d1e34003
Shane Warner <shane.warner2@fox.com>
2019-11-11 17:19:16 -0800

    fix serial port / add ch376 support

--- Marlin/advi3pp.cpp
+++ Marlin/advi3pp.cpp
@@ -23,6 +23,9 @@
 #include "serial.h"
 #include "temperature.h"
 #include "cardreader.h"
+#if ENABLED(CH376_STORAGE_SUPPORT)
+#include "mass_storage/cardusbdiskreader.h"
+#endif
 #include "planner.h"
 #include "parser.h"
 #include "printcounter.h"
@@ -128,7 +131,7 @@ int16_t scale(int16_t value, int16_t valueScale, int16_t targetScale)
 //! Initialize the printer and its LCD
 void ADVi3pp_::setup_lcd_serial()
 {
-    Serial2.begin(advi3_pp_baudrate);
+    Serial3.begin(advi3_pp_baudrate);
 }
 
 //! Change the baudrate. Check before that it is actually different.
